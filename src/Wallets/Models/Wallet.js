79db26e0e43f75d4ab7786394f9a8a752cbec4b9
Karl Buys <kbbuys@gmail.com>
2022-09-27 13:38:12 +0200

    fix: hardcode needsmport to false

--- src/Wallets/Models/Wallet.js
+++ src/Wallets/Models/Wallet.js
@@ -1250,7 +1250,7 @@ class Wallet extends EventEmitter {
 		{
 		  fromWallet_didFailToInitialize: self.didFailToInitialize_flag == true,
 		  fromWallet_didFailToBoot: self.didFailToBoot_flag == true,
-		  fromWallet_needsImport: self.shouldDisplayImportAccountOption == true,
+		  fromWallet_needsImport: false,
 		  requireAuthentication: self.context.settingsController.authentication_requireWhenSending != false,
 		  //
 	          destinations: destinations, 
